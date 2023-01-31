43e1dd65a70c8de04f733fc68292fcb65459821d
codescalper <msfunbook@gmail.com>
2023-01-31 22:05:38 +0530

    Module 3.1

---
+++ Functional & Oops/var_let_const.js
@@ -0,0 +1,29 @@
+function meal(animal) {
+    animal.food = animal.food + 10;
+}
+
+var dog = {
+    food: 10
+};
+meal(dog);
+meal(dog);
+
+console.log(dog.food);
+
+
+//about parameters
+//giving the correct order of the parameter is very important 
+//below example shows that
+function two() {
+    return 2;
+}
+
+function one() {
+    return 1;
+}
+
+function calculate(initialValue, incrementValue) {
+    return initialValue() + incrementValue() + incrementValue();
+}
+
+console.log(calculate(two, one));
